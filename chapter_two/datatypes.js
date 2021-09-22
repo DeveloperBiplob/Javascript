fb76e0d44670cc2f5d1ed78548bda8cbd0113ee8
BIPLOB <developerbiplob.bd@gmail.com>
2021-09-23 03:07:30 +0600

    Number Data Types | Infinity | NAN

---
+++ chapter_two/datatypes.js
@@ -0,0 +1,39 @@
+// Primitive Data Type
+    // Number
+    // 10, 10.5, 66,8
+
+    //String
+    // "this is a text" + 'this is text'
+
+    // Boolean 
+    // True or false
+
+    // Undefined
+    // Null
+
+// Object Data Type
+    // Array
+    // Object
+    // Function
+
+ // *********** NUMBERS ***************
+
+var n = 1421
+var f = 3.76
+var nn = Number('45')
+console.log(nn)
+console.log(Number.parseFloat(nn))
+console.log(Number.parseInt(nn))
+
+// Infinity Data type
+console.log(1/0)
+
+// NAN Data types
+console.log('abc' * 10)
+
+
+
+
+
+
+
