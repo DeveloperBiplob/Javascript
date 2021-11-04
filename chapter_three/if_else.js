7975280634f73c60d7c1fc998ed7b1e2eb8eb041
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-04 13:09:50 +0600

    If Else And Else IF Condition

---
+++ chapter_three/if_else.js
@@ -0,0 +1,39 @@
+//------- If Else And ElseIf Conditrion ----------//
+
+var a = 10
+var b = 20
+
+if (a > b) {
+    console.log('A is greater thaan B')
+}else{
+    console.log('B is greater thaan A')
+}
+
+var c = 5
+
+if (c % 2 == 0) {
+    console.log("Evne")
+}else{
+    console.log("odd")
+}
+
+var a = 20
+var b = 20
+
+if(a > b){
+    console.log('A is greater thaan B')
+}else if(a < b){
+    console.log('B is greater thaan A')
+}else{
+    console.log('They both are same')
+}
+
+var c = 0
+
+if(c == 0){
+    console.log(c + " is Zero")
+}else if (c % 2 == 0) {
+    console.log("Evne")
+}else{
+    console.log("odd")
+}
