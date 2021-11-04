160a442d34347809edaea46606ce34d08c57cf8f
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-04 12:24:57 +0600

    Date Functions

---
+++ chapter_two/date.js
@@ -0,0 +1,14 @@
+var date = new Date()
+
+console.log(date) // 2021-11-04T05:57:24.543Z
+console.log(date.toDateString()) // Thu Nov 04 2021
+console.log(date.toTimeString()) // 11:59:35 GMT+0600 (Bangladesh Standard Time)
+console.log(date.toLocaleString()) // 11/4/2021, 11:59:35 AM
+console.log(date.toLocaleDateString()) // 11/4/2021
+
+console.log(date.getFullYear()) // Year print kore 2021
+console.log(date.getMonth()) // Month print kore 10
+console.log(date.getDate()) // Dtete print kore 10
+console.log(date.getHours()) 
+console.log(date.getMinutes())
+console.log(date.getSeconds()) 
