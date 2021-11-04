9257e29ca9f0d6c9d87d61467ede8717d02c4482
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-04 11:10:27 +0600

    Operatores | like Arithmetic Operator, Comparison Operatrors

---
+++ chapter_two/operator.js
@@ -0,0 +1,64 @@
+// ---------- Operators --------------//
+
+// var a = 11
+// var b = 6
+
+//## Arithmetic Operator
+// + - * / % ++ --
+
+// console.log(a % b) 
+
+//## Incemental 
+// Pre Incremental - (++a) 
+// Post Incremental - (a++)
+
+// console.log(a++)
+// console.log(++a)
+
+//## Assignment Opetrator
+// var a = 10
+// var b = 20
+
+// a = a+b
+// console.log(a)
+
+// a += b
+// console.log(a)
+
+// a -= b
+// console.log(a)
+
+// a *= b
+// console.log(a)
+
+// a /= b
+// console.log(a)
+
+// a %= b
+// console.log(a)
+
+
+//-------- Comparison Operatrors(==)--------//
+// var a = 10
+// var b = 20
+
+// console.log(a == b) // false
+// console.log(a !== b) // true
+
+// console.log(a > b) // false
+// console.log(a < b) // true
+
+// console.log(a >= b) // false
+// console.log(a <= b) // true
+
+
+// var c = '10'
+// var d = 10
+
+// console.log(c === d)
+// console.log(c !== d)
+
+// Logical Operators
+// &&
+// ||
+// !
