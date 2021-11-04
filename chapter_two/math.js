1e97f8838ac32de11f1808a3c5eb07ae59c7c815
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-04 11:54:09 +0600

    Math Functions | Like- Max, Min, Floor, Etc

---
+++ chapter_two/math.js
@@ -0,0 +1,24 @@
+//-------Math Function----------//
+
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 4.564
+console.log(Math.abs(n)) // return to Absulate value
+
+console.log(Math.floor(n)) // . er porer gulo remove kore.
+
+console.log(Math.ceil(n)) // 
+
+console.log(Math.round(n)) // kacha kachi number e convert kore dei.
+
+console.log(Math.max(500, 400, 300))
+
+console.log(Math.min(500, 400))
+
+console.log(Math.pow(2, 3)) // Power ber kore
+
+console.log(Math.sqrt(9)) // Squre ber kore
+
+console.log(Math.random()) // Power ber kore
+console.log(Math.round(Math.random() * 50 + 1)) // 1 - 50 er modde random number make korbe
