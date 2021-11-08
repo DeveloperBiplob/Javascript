b7decc5dbd09af9b351a5800119af8e7d581a438
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-08 11:36:22 +0600

    For loop

---
+++ chapter_four/forloop.js
@@ -0,0 +1,76 @@
+//-----------For Loop-------------//
+
+// for(initializer, condition, increment/dicrement){
+//     output
+// }
+
+// for(var i = 0; i <= 100; i++){
+//     console.log('Biplob jabery')
+// }
+
+
+// The name is print repeted 100-------//
+// for(var i = 0; i <= 100; i++){
+//     console.log((i + 1) + ' Biplob jabery')
+// }
+
+
+// 1 - 100 print ------//
+// for(var i = 0; i <= 100; i++){
+//     console.log(i)
+// }
+
+
+// 1 - 100 Odd Number print-----//
+// for(var i = 0; i <= 100; i++){
+//     if(i % 2 == 1){
+//         console.log(i)
+//     }
+// }
+
+
+// 1 - 100 Even Number print-----//
+// for(var i = 0; i <= 100; i++){
+//     if(i % 2 == 0){
+//         console.log(i)
+//     }
+// }
+
+
+// 1 - 100 number sum----//
+var sum = 0
+// for(var i = 1; i <= 100; i++){
+//     // sum = sum + i
+//     sum+= i
+// }
+// console.log(sum)
+
+
+// for(var i = 1; i <= 10; i++){
+//     console.log(sum + " + " + i + " = " + (sum + i))
+//     sum += i
+// }
+// console.log('Result = ' + sum)
+
+
+
+// Practice-----
+
+// odd number sum
+
+var sum = 0
+// for(var i = 1; i <= 100; i ++){
+//     if(i % 2 == 1){
+//         sum += i
+//     }
+// }
+// console.log(sum)
+
+// 5 diye bag jay
+// for(var i = 1; i <= 100; i++){
+//     if(i % 5 == 0){
+//         sum += i
+//     }
+// }
+// console.log(sum)
+
