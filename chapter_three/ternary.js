143857649d835fd7511b33c76b22b527b80c7d9f
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-08 09:58:57 +0600

    Ternary Operator

---
+++ chapter_three/ternary.js
@@ -0,0 +1,19 @@
+//----------------- Ternary Operator-----------------//
+var n = 11 
+var str = ''
+
+if(n % 2 == 0){
+    str = 'EVEN'
+}else{
+    str = 'ODD'
+}
+console.log(str)
+
+
+
+// Condition ? true side : false side
+
+var result = n % 2 == 0 ? 'EVEN' : 'ODD'
+console.log(result)
+
+console.log( n % 2 == 0 ? 'EVEN' : 'ODD' )
