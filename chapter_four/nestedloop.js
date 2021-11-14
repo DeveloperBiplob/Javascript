eb3f3169acc43ea3bc33d71f3dddcaaf10191aef
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-14 12:55:40 +0600

    Nested loop

---
+++ chapter_four/nestedloop.js
@@ -0,0 +1,40 @@
+// --------- Nested Loop ----------//
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// Nested loop use kore ai out put ta show korbo.
+
+// var num = 5
+// for (var i = 1; i <= num; i ++){
+
+//     var result = ''
+//     for(var x = 1; x <= i; x ++){
+//         // console.log(x)
+//         result += x + " "
+//     }
+//     console.log(result)
+// }
+
+// * * * * * *
+// * * * * * *
+// * * * * * *
+// * * * * * *
+// * * * * * *
+// * * * * * *
+
+// ai ta print korbo loop use kore.
+
+var num = 10
+for(var i= 1; i <= num; i ++){
+    var result = ''
+    for(var j = 1; j <= num; j ++){
+        // result += '* '
+        result = result + '* '
+    }
+
+    console.log(result)
+}
