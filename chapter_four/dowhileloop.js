6ce127dbff87232b49810e7d16731c7ccd66d348
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-14 12:28:20 +0600

    Do While loop

---
+++ chapter_four/dowhileloop.js
@@ -0,0 +1,14 @@
+//----------Do While Loop---------//
+
+var isRunning = false
+
+while(isRunning){
+    console.log('I am Running')
+}
+
+// do while loop e first time conditrion check na kore i loop ta ak bar cholbe.
+// tar por condition joto khon true thakbe loop toto khon running thakbe.
+
+do{
+    console.log('I am running')
+}while(isRunning)
