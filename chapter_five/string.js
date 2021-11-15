46172042b292b121022521b2931136bc465249dd
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-15 11:31:45 +0600

    String Literal vs Constructor

---
+++ chapter_five/string.js
@@ -0,0 +1,9 @@
+//-------- String -----------//
+
+// var str = 'Something' // string literal
+// var str2 = String('something') // string constructor
+
+// var n = 10
+// console.log(String(n))
+
+
