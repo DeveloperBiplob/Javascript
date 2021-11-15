d6364b04d31dc27417820f3d96e5da26219e0e17
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-15 11:41:58 +0600

    Escape Notation

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -7,3 +7,13 @@
 // console.log(String(n))
 
 
+//**** Escape Notation
+
+// var str = "This is a 'String'"
+
+// var str2 = "This is a \'String\'" // Escape Notation. \' \' = cotetion print kore
+// var str3 = "This is a \nString" // Escape Notation.  \n = new line create kore
+// var str3 = "This is a \tString" // Escape Notation.  \t = tabe create kore
+// var str4 = "This is a \\String" // Escape Notation.  \ = \ print kore
+
+// console.log(str3)
