668032d246971e8b7a1ec9bbbd400178c62d9da8
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-15 12:03:14 +0600

    String Comparison

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -17,3 +17,20 @@
 // var str4 = "This is a \\String" // Escape Notation.  \ = \ print kore
 
 // console.log(str3)
+
+
+
+//******** String Comparison ******* //
+// Javascript Use Lexicographic System to Compare to String.
+
+// a, b, c ++ ai vabe jeta age seta boro, tar mane holo 'b' theke 'a' boro.
+var a = 'abc'
+var b = 'bcd'
+
+console.log(a < b)
+
+// je kono small later Capital later e chaye boro.
+console.log('z' > 'Z')
+
+// string er sathe number compare korle string ta numbere convert kore compayer korbe.
+console.log('001' == 1) // true return korbe.
