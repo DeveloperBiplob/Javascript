3471371dd4e056ec20ab0d9186c0b3ffc4ef9f23
BIPLOB <developerbiplob.bd@gmail.com>
2021-11-15 13:45:34 +0600

    String Method

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -24,13 +24,36 @@
 // Javascript Use Lexicographic System to Compare to String.
 
 // a, b, c ++ ai vabe jeta age seta boro, tar mane holo 'b' theke 'a' boro.
-var a = 'abc'
-var b = 'bcd'
+// var a = 'abc'
+// var b = 'bcd'
 
-console.log(a < b)
+// console.log(a < b)
 
 // je kono small later Capital later e chaye boro.
-console.log('z' > 'Z')
+// console.log('z' > 'Z')
 
 // string er sathe number compare korle string ta numbere convert kore compayer korbe.
-console.log('001' == 1) // true return korbe.
+// console.log('001' == 1) // true return korbe.
+
+
+//******** String Method *******/
+// concate() // akadik string ke jora lagay
+// substr() // koto number theke show korbe r koto ta show korbe. secnond peramiter optional.
+// charAt() // kono index e kon value ta ache seta dekar jonno.
+// startWith() // true, flase return kore. console.log(c.startsWith('I')) 
+                //jodi string ta 'I' diye suro hoy ta hole true return korbe noyto false return korbe.
+// endWith() // condition check korbe pass kora value diye ses hoiche kina.
+// toUpperCase()
+// toLowerCase()
+// trim() // spase remove kore. "    biplob   " ai rokom
+// split() // ata kono akta string ke benge array kore, r seta kisher upor vitti kore bangbo ta bole dite hobe.
+
+// var a = 'I am'
+// var b = 'Biplob Jabery'
+// var c = a.concat(' ', b)
+// console.log(c)
+// console.log(c.substr(5))
+// console.log(c.charAt(6))
+// console.log(c.startsWith('I'))
+// console.log(c.startsWith('I am'))
+// console.log(c.split(' '))
